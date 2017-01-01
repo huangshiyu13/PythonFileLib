0a06452091d493c4046a8b42715f8af0d6dfbc10
hsy <huangsy13@gmail.com>
2017-01-01 14:30:00 +0800

    update

--- filelib.py
+++ filelib.py
@@ -1,10 +1,18 @@
-def getAllFiles(dirName, houzhui):
+import os
+
+def getAllFiles(dirName, houzhui=' '):
     results = []
+
     for file in os.listdir(dirName):
         file_path = os.path.join(dirName, file)
-        if os.path.isfile(file_path) and os.path.splitext(file_path)[1] == houzhui:
-            results.append(file_path)
+        if houzhui == ' ':
+            if os.path.isfile(file_path):
+                results.append([file_path, os.path.splitext(file)[0]])
+        else:
+            if os.path.isfile(file_path) and os.path.splitext(file_path)[1] == houzhui:
+                results.append([file_path,os.path.splitext(file)[0]])
     return results
+
 
 def checkFile(fileName):
     if os.path.isfile(fileName):
@@ -25,3 +33,9 @@ def checkDir(fileName, creat=False):
         else:
             print fileName, 'is not found!'
             exit()
+
+
+if __name__ == '__main__':
+    checkFile('filelib.py')
+    checkDir('./.git')
+    print getAllFiles('./')
